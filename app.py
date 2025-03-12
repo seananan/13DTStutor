281395c895a7347d8d6c129e77af0ebbac2f5cc5
Sean Hurst <21236@wc.school.nz>
2025-03-13 11:09:12 +1300

    Almost finished sign up and log in stuff

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 from sqlite3 import Error
 
+DATABASE = 'DB_FILE'
+
 app = Flask(__name__)
 
 def connect_database(db_file):
@@ -10,8 +12,6 @@ def connect_database(db_file):
     :param db_file:
     :return: conn
     """
-
-
     try:
         connection = sqlite3.connect(db_file)
         return connection
@@ -42,8 +42,17 @@ def render_signup_page():  # put application's code here
         if len(password1) < 8:
             return redirect("\signup?error=password+must+be+over+8+characters")
 
+        con = connect_database(DATABASE)
+        query_insert = "INSERT INTO user (fname, lname, email, password1) VALUES (?. ?, ?, ?)"
+        cur = con.cursor()
+        cur.execute(query_insert, (fname, lname, email, password1))
+        con.commit()
+        con.close()
     return render_template('signup.html')
 
+@app.route('/booking',methods=['POST', 'GET'])
+def render_booking_page():  # put application's code here
+    return render_template('booking.html')
 
 if __name__ == '__main__':
     app.run()
